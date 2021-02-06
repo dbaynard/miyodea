34c1f5b5de1d37d537e91320fe2d6e716649c22c
David Baynard <david@baynard.dev>
2021-02-06 16:07:41 +0000

    Switch page to tabbed view

--- miyodea-app/src/App.tsx
+++ miyodea-app/src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { Tabs, Tab } from "react-bootstrap";
+
 import "./App.css";
+
 import { Workouts } from "./Workouts";
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <h1>Mi Yodea</h1>
-      <h2>
-        <i>a.k.a.</i> 12 Days of Christmas workout
-      </h2>
-      <p>Who knows one?</p>
-    </header>
-    <Workouts />
+    <Tabs>
+      <Tab eventKey="home" title="Home">
+        <header className="App-header">
+          <h1>Mi Yodea</h1>
+          <h2>
+            <i>a.k.a.</i> 12 Days of Christmas workout
+          </h2>
+          <p>Who knows one?</p>
+        </header>
+      </Tab>
+      <Tab eventKey="workouts" title="Workouts">
+        <Workouts />
+      </Tab>
+    </Tabs>
   </div>
 );
 
