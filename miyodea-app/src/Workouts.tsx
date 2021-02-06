91fc75c4c5cd84d8cd83ea07fd3d56835c129f1c
David Baynard <david@baynard.dev>
2021-02-06 04:19:38 +0000

    Tabulate workouts

--- miyodea-app/src/Workouts.tsx
+++ miyodea-app/src/Workouts.tsx
@@ -1,6 +1,40 @@
 import React from "react";
+import DataTable from "react-data-table-component";
+
 import workouts from "./workouts.json";
 
-const Workouts = () => <>{JSON.stringify(workouts)}</>;
+type Workout = {
+  name: string;
+  targets: string[];
+  variants?: string[];
+  equipment?: string;
+};
+
+const columns = [
+  { name: "Workout", selector: "name" },
+  {
+    name: "Targets",
+    selector: "targets",
+    format: (row: Workout) => row.targets.join(", "),
+  },
+  {
+    name: "Variants",
+    selector: "variants",
+    format: (row: Workout) => row.variants?.join(", "),
+    wrap: true,
+    style: { textAlign: "left" },
+  },
+  { name: "Equipment", selector: "equipment" },
+];
+
+const Workouts = () => (
+  <DataTable
+    title="Workouts"
+    columns={columns}
+    data={workouts}
+    keyField="name"
+    striped
+  />
+);
 
 export { Workouts };
