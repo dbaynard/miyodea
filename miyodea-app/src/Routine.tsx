000959ef2ce7f8909fc64e09d4b68500dbcc7180
David Baynard <david@baynard.dev>
2021-02-07 02:22:04 +0000

    Wrap routine in container

--- miyodea-app/src/Routine.tsx
+++ miyodea-app/src/Routine.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Badge, ListGroup } from "react-bootstrap";
+import { Container, Badge, ListGroup } from "react-bootstrap";
 
 import { Workout } from "./Workouts";
 
@@ -15,11 +15,16 @@ const Entry = ({ n, name }: EntryProps) => (
 export type RoutineProps = { workouts: Workout[] };
 
 const Routine = ({ workouts }: RoutineProps) => (
-  <ListGroup as="ol">
-    {workouts
-      .map(({ name }, n) => <Entry n={n + 1} {...{ name }} />)
-      .slice(0, 12)}
-  </ListGroup>
+  <Container>
+    <header>
+      <h1>Routine</h1>
+    </header>
+    <ListGroup as="ol">
+      {workouts
+        .map(({ name }, n) => <Entry n={n + 1} {...{ name }} />)
+        .slice(0, 12)}
+    </ListGroup>
+  </Container>
 );
 
 export { Routine };
