c48cc48bf87ac35f31f2f920452340b46646ba84
David Baynard <david@baynard.dev>
2021-02-07 02:42:58 +0000

    Customize number of rounds

--- miyodea-app/src/Routine.tsx
+++ miyodea-app/src/Routine.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Col,
   Row,
@@ -11,46 +11,59 @@ import {
 
 import { Workout } from "./Workouts";
 
-type EntryProps = { n: number; name: string };
+type EntryProps = { rounds: number; n: number; name: string };
 
-const Entry = ({ n, name }: EntryProps) => (
+const Entry = ({ rounds, n, name }: EntryProps) => (
   <ListGroup.Item>
     <Badge variant="primary">{n}</Badge> {name}{" "}
-    <Badge variant="light">×{n * (13 - n)}</Badge>
+    <Badge variant="light">×{n * (rounds + 1 - n)}</Badge>
   </ListGroup.Item>
 );
 
 export type RoutineProps = { workouts: Workout[] };
 
-const Routine = ({ workouts }: RoutineProps) => (
-  <Container>
-    <header>
+const defaultRounds = 12;
+
+const Routine = ({ workouts }: RoutineProps) => {
+  const [rounds, setRounds] = useState<number>(defaultRounds);
+
+  return (
+    <Container>
+      <header>
+        <Row>
+          <Col>
+            <h1>Routine</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="auto">Total {0.5 * rounds * (rounds + 1)}</Col>
+          <Col md="auto">
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>#</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="12"
+                htmlSize={2}
+                onChange={({ target }) =>
+                  setRounds(Number.parseInt(target?.value) || defaultRounds)
+                }
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+      </header>
       <Row>
         <Col>
-          <h1>Routine</h1>
+          <ListGroup as="ol">
+            {workouts
+              .map(({ name }, n) => <Entry n={n + 1} {...{ name, rounds }} />)
+              .slice(0, rounds)}
+          </ListGroup>
         </Col>
       </Row>
-      <Row>
-        <Col md="auto">
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text>#</InputGroup.Text>
-            </InputGroup.Prepend>
-            <FormControl placeholder="12" htmlSize={2} />
-          </InputGroup>
-        </Col>
-      </Row>
-    </header>
-    <Row>
-      <Col>
-        <ListGroup as="ol">
-          {workouts
-            .map(({ name }, n) => <Entry n={n + 1} {...{ name }} />)
-            .slice(0, 12)}
-        </ListGroup>
-      </Col>
-    </Row>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export { Routine };
